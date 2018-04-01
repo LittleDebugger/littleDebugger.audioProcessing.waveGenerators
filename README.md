06d7d2b76577537ace9b65997d6b1f46102ef96b
Hedley Gisbourne <hedley.gisbourne@icloud.com>
2018-04-01 23:41:02 +0200

    SineWave Generator
    First commit of submodule.
    * Includes sinewave generator
    * Changed namespace of generators

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # com.littleDebugger.audioProcessing.waveGenerators
+
+A reusable module to contain audio waveform generators.
+
+Design decissions
+* The sine wave generator previously had functionality to set the phase offset when initializing or reseting. This has been removed since its not likely to be needed for audio processing and introduces an overhead. This might be addresssed later and added again if there is a need.
+* The sinewave will not be perfect since it now uses a lookup which involves rounding. The performance increase by doing it this way is substantial and the noise is insignificant so its (currently) a worthwhile tradeoff.
---
+++ sineWave.js
@@ -0,0 +1,34 @@
+// The sine (sinusoidal) wave generator.
+com.littleDebugger.namespacer.createNamespace("com.littleDebugger.audioProcessing.waveGenerators.sineWave");
+
+// 'Constructor'
+// <offset> The offset of the initial phase in degrees. - Starting from 9 o'clock.
+// <sampleRate> The sample rate of the audio player. This is required for correct frequency waves.
+com.littleDebugger.audioProcessing.waveGenerators.sineWave = function(offset, sampleRate) {
+    var circleRadians = 2 * Math.PI;
+    var circleDegrees = 360;
+
+    var that = {};
+    var phase;
+
+    // Reset the state of the generator so it can be reused.
+    that.reset = function() {
+        phase = circleRadians * (offset / circleDegrees);
+    };
+
+    // Get the next sample in the cycle.
+    // <freqency> The frequency of the wave.
+    that.getSample = function(frequency) {
+        var nextValue = Math.sin(phase);
+        phase += circleRadians * (frequency / sampleRate);
+        if (phase > circleRadians) {
+            phase = phase % circleRadians;
+        }
+
+        return nextValue;
+    };
+
+    that.reset();
+
+    return that;
+};
